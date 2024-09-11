7ca988c9b998a3465a54f62a75439254b063f96e
zahar <zman-96@mail.ru>
2024-09-11 10:40:13 +0300

    comments

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -12,28 +12,32 @@ router = Router()
 @router.message(Command(commands='create_wallet'))
 async def create_wallet(message: Message):
     if message.from_user.id == admin_id:
+        wait_message = await message.answer('Подождите пожалуйста, соединение с сетью требует немного времени...')
         try:
             my_wallet = Wallet.create(wallet_name, network=network)
-            address = my_wallet.get_key().address
+            address = my_wallet.addresslist()[0]
             await bot.send_message(admin_id, f'Создан кошелёк с адресом {address}')
-
+            await wait_message.delete()
         except WalletError:
             wallet = Wallet(wallet_name)
-            addr = wallet.get_key().address
+            addr = wallet.addresslist()[0]
             await bot.send_message(admin_id, f'кошелек уже создан с адресом:\n'
                                              f'{addr}')
-
+            await wait_message.delete()
 
 @router.message(Command(commands='get_balance'))
 async def get_balance(message: Message):
     if message.from_user.id == admin_id:
+        wait_message = await message.answer('Подождите пожалуйста, соединение с сетью требует немного времени...')
         try:
             wallet = Wallet(wallet_name)
             wallet.utxos_update()
             balance = wallet.balance_update_from_serviceprovider()
-            await bot.send_message(admin_id, f'{balance}')
+            await bot.send_message(admin_id, f'{balance} sat (баланс обновляется после подтверждения транзакций в блокчейне сети)')
+            await wait_message.delete()
         except WalletError:
-             await bot.send_message(chat_id=admin_id, text='Кошелек не создан')
+            await bot.send_message(chat_id=admin_id, text='Кошелек не создан')
+            await wait_message.delete()
 
 
 @router.message(F.text == 'admin')
--- handlers/drop_handler.py
+++ handlers/drop_handler.py
@@ -18,6 +18,7 @@ async def send_drop(message: Message):
     if user_id in time_manager and (int(time()) - time_manager[user_id]) < pause_time:
         await bot.send_message(user_id, f'Вы слишком часто запрашиваете транзакцию, подождите {pause_time - (int(time()) - time_manager[user_id])} секунд')
     else:
+        wait_message = await message.answer('Подождите пожалуйста, соединение с сетью требует немного времени...')
         try:
             wallet = Wallet(wallet_name)
             wallet.utxos_update()
@@ -27,11 +28,11 @@ async def send_drop(message: Message):
             await bot.send_message(admin_id, f'проведена транзакция {tr}\n'
                                             f'на адрес {message.text}\n'
                                             f'пользователем {user}, {user_id}')
-
+            await wait_message.delete()
         except WalletError:
             await message.answer('извините, транзакция не удалась')
             await bot.send_message(admin_id, 'недостаточно средств для транзакции')
-
+            await wait_message.delete()
         time_manager[user_id] = int(time())
 
 
